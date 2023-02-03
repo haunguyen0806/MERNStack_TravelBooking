397d635c2fbed8756c5a16c0f124f1656275fb0f
haunguyen <haunguyen.08061999@gmail.com>
2023-02-03 14:35:26 +0700

    dbconfig

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+mongoose.set("strictQuery",false)
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log('MongoDB database connected')
+    } catch (err) {
+        console.log("MongoDB database connection failed")
+    }
+}
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 8000
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.listen(port, () =>{
+    connect()
+    console.log('Server listening on port', port)
+})
